b5ce35ef6dde9b13f764f0a92589e8984000ea3c
Nitro Development <66451996+Nitr0Dev@users.noreply.github.com>
2020-12-22 22:54:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('</> By Nitro#0177 w/ ❤'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`This app is listining on http://localhost:${port}`));
