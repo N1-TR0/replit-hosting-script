3a8070bdef0dffebe86573b1027f61d3100d1e32
N1TR0 <66451996+N1-TR0@users.noreply.github.com>
2021-05-08 17:45:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('</> By Aunixinq#0117 w/ ❤'));
+app.get('/', (req, res) => res.send('</> By Nitro#0117 w/ ❤'));
 
 app.listen(port, () => console.log(`This app is listining on http://localhost:${port}`));
